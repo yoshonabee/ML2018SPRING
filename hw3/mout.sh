89b30e276c4f5959ce79ed25f413dca920ff313e
yoshonabee <jhann0120@gmail.com>
2018-04-08 11:25:04 +0800

    hw3_trans1

---
+++ hw3/mout.sh
@@ -0,0 +1,6 @@
+cp ~/Desktop/ml/ML2018SPRING/hw3/hw3_train.py ~/Desktop/ml
+cp ~/Desktop/ml/ML2018SPRING/hw3/hw3_train.sh ~/Desktop/ml
+cp ~/Desktop/ml/ML2018SPRING/hw3/train.py ~/Desktop/ml
+cp ~/Desktop/ml/ML2018SPRING/hw3/test.py ~/Desktop/ml
+cp ~/Desktop/ml/ML2018SPRING/hw3/min.sh ~/Desktop/ml
+cp ~/Desktop/ml/ML2018SPRING/hw3/mout.sh ~/Desktop/ml
