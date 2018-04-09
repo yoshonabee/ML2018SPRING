6b3138c6e168686f5aa44be885eefc4e1416ab13
yoshonabee <jhann0120@gmail.com>
2018-04-09 23:24:29 +0800

    t

---
+++ hw3/hw3_fc.py
@@ -0,0 +1,31 @@
+import csv
+import numpy as np
+from keras.utils import np_utils
+
+def loadData(filename, mode):
+	x = []
+	y = []
+	f = open(filename, 'r')
+	row = csv.reader(f, delimiter=' ')
+	n_row = 0
+	for r in row:
+		if n_row != 0:
+			temp = []
+			for i in range(len(r)):
+				if i == 0 and mode == 'train':
+					y.append(int(r[i][0]))
+					temp.append(int(r[i][2:]))
+				else:
+					temp.append(int(r[i]))
+			x.append(temp)
+		n_row += 1
+	f.close()
+
+	x = np.array(x)
+	y = np.array(y)
+	y = np_utils.to_categorical(y, num_classes=7)
+	x = x.reshape(x.shape[0], 48, 48, 1)
+	x = x / 255
+
+	if mode == 'train': return x, y
+	else: return x
--- min.sh
+++ min.sh
@@ -2,3 +2,6 @@ cp ~/Desktop/ml/hw3_train.py ~/Desktop/ml/ML2018SPRING/hw3/
 cp ~/Desktop/ml/hw3_train.sh ~/Desktop/ml/ML2018SPRING/hw3/
 cp ~/Desktop/ml/train.py  ~/Desktop/ml/ML2018SPRING/hw3/
 cp ~/Desktop/ml/test.py ~/Desktop/ml/ML2018SPRING/hw3/
+cp ~/Desktop/ml/mout.sh  ~/Desktop/ml/ML2018SPRING/hw3/
+cp ~/Desktop/ml/min.sh ~/Desktop/ml/ML2018SPRING/hw3/
+cp ~/Desktop/ml/hw3_fc.py ~/Desktop/ml/ML2018SPRING/hw3/
