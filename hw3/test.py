0e0946e7cc70d6d3d02dc6bba68b2ba1d3c8e062
yoshonabee <jhann0120@gmail.com>
2018-04-09 23:20:33 +0800

    t

--- hw3/test.py
+++ hw3/test.py
@@ -1,2 +1,21 @@
-from tensorflow.python.client import device_lib
-print(device_lib.list_local_devices())
+import os
+import sys
+import numpy as np
+from keras.utils import *
+from keras.models import Sequential, load_model
+from keras.layers import Dense, Flatten, Dropout, Conv2D, MaxPooling2D, Activation
+from keras.optimizers import Adam
+from hw3_fc import loadData
+
+os.environ["CUDA_VISIBLE_DEVICES"] = '0'
+from keras.backend.tensorflow_backend import set_session
+config = tf.ConfigProto()
+gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=1)
+set_session(tf.Session(config=config))
+
+model = load_model('model.h5')
+x_test = loadData(sys.argv[1], 'test')
+
+y_test = model.predict(x_test)
+for i in y_test:
+	print(i)
--- hw3/train.py
+++ hw3/train.py
@@ -1,12 +1,11 @@
 import os
 import sys
-import csv
 import numpy as np
-import tensorflow as tf
 from keras.utils import *
 from keras.models import Sequential
 from keras.layers import Dense, Flatten, Dropout, Conv2D, MaxPooling2D, Activation
 from keras.optimizers import Adam
+from hw3_fc import loadData
 
 os.environ["CUDA_VISIBLE_DEVICES"] = '0'
 from keras.backend.tensorflow_backend import set_session
@@ -14,31 +13,6 @@ config = tf.ConfigProto()
 gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=1)
 set_session(tf.Session(config=config))
 
-def loadData(filename, mode):
-	x_train = []
-	y_train = []
-	f = open(filename, 'r')
-	row = csv.reader(f, delimiter=' ')
-	n_row = 0
-	for r in row:
-		if n_row != 0:
-			temp = []
-			for i in range(len(r)):
-				if i == 0 and mode == 'train':
-					y_train.append(int(r[i][0]))
-					temp.append(int(r[i][2:]))
-				else:
-					temp.append(int(r[i]))
-			x_train.append(temp)
-		n_row += 1
-	f.close()
-
-	x_train = np.array(x_train)
-	y_train = np.array(y_train)
-	y_train = np_utils.to_categorical(y_train, num_classes=7)
-	x_train = x_train.reshape(x_train.shape[0], 48, 48, 1)
-	x_train = x_train / 255
-	return x_train, y_train
 
 x_train, y_train = loadData(sys.argv[1], 'train')
 np.save("x_train.npy", x_train)
@@ -70,6 +44,6 @@ if (input("\nRun? Y/n: ") == 'y'):
 	adam = Adam(lr=0.001)
 	model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=['accuracy'])
 	train_history = model.fit(x_train, y_train, validation_split=0.2, batch_size=500, epochs=epoch)
-
+	model.save('model.h5')
 # result = model.evaluate(x_train, y_train, batch_size=100000)
 # print('\nTest Accuracy:', result[1])
